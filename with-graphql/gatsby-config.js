39e525c26620e5e9ece66d687d1bc54c5277cb25
Rafał Kucharski <rafal.kucharski@thesoftwarehouse.pl>
2018-03-04 01:01:18 +0100

    Added generation of content from markdown files

--- with-graphql/gatsby-config.js
+++ with-graphql/gatsby-config.js
@@ -1,8 +1,17 @@
+//     `gatsby-transformer-remark`, - converts md files
 module.exports = {
   siteMetadata: {
     title: 'Examplary page title',
   },
   plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    `gatsby-transformer-remark`,
     `gatsby-plugin-glamor`,
     {
       resolve: `gatsby-plugin-typography`,
--- with-graphql/src/pages/index.js
+++ with-graphql/src/pages/index.js
@@ -1,13 +1,44 @@
 import React from "react";
+import g from "glamorous";
+import { rhythm } from "../utils/typography";
 
-export default () => (
+export default ({data}) => (
   <div>
-    <h1>Some kind of HOME title</h1>
-    <div>
-      <img
-        src="https://2.bp.blogspot.com/-BMP2l6Hwvp4/TiAxeGx4CTI/AAAAAAAAD_M/XlC_mY3SoEw/s1600/panda-group-eating-bamboo.jpg"
-        alt="some image"
-      />
-    </div>
-  </div>
+     <g.H1 display={"inline-block"} borderBottom={"1px solid"}>
+       Amazing Pandas Eating Things
+     </g.H1>
+     <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+     {data.allMarkdownRemark.edges.map(({ node }) => (
+       <div key={node.id}>
+         <g.H3 marginBottom={rhythm(1 / 4)}>
+           {node.frontmatter.title}{" "}
+           <g.Span color="#BBB">— {node.frontmatter.date}</g.Span>
+         </g.H3>
+         <p>{node.excerpt}</p>
+       </div>
+     ))}
+   </div>
 );
+
+// allMarkdownRemark is pulled from
+//      gatsby-source-filesystem
+// and is filtered by
+//      gatsby-transformer-remark
+
+export const query = graphql`
+  query IndexQuery {
+    allMarkdownRemark(sort: {fields: [frontmatter___date], order: DESC}) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`;
